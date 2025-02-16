630afbeebf3cb793ab0310fb30a7dd3e9f7354f8
ARMontt <alan.r.montt@outlook.com>
2025-02-15 20:28:51 -0500

    CPU library added

---
+++ include/cpu.h
@@ -0,0 +1,20 @@
+
+
+#pragma once
+
+#include<cstdint>
+#include "bus.h"
+
+class CPU{
+  public:
+    uint64_t regs[32];
+    uint64_t pc;
+    BUS bus;
+
+    void cpu_init(CPU* cpu);
+    uint32_t cpu_fetch(CPU* cpu);
+    int cpu_execute(CPU* cpu, uint32_t inst);
+    void dump_registers(CPU* cpu);
+    uint64_t cpu_load(CPU* cpu, uint64_t addr, uint64_t size);  
+    void cpu_store(CPU* cpu, uint64_t addr, uint64_t size, uint64_t value);
+};
---
+++ src/cpu.cpp
@@ -0,0 +1,21 @@
+
+#include "cpu.h"
+
+void CPU::cpu_init(CPU* cpu){
+  cpu->regs[0] = 0x00;
+  cpu->regs[2] = DRAM_BASE + DRAM_SIZE;
+  cpu->pc      = DRAM_BASE;
+}
+
+uint32_t CPU::cpu_fetch(CPU* cpu){
+  uint32_t inst = bus.bus_load(&(cpu->bus), cpu->pc,32);
+  return inst;
+}
+
+uint64_t CPU::cpu_load(CPU* cpu, uint64_t addr, uint64_t size){
+  return bus.bus_load(&(cpu->bus),addr,size);
+}
+
+void CPU::cpu_store(CPU* cpu, uint64_t addr, uint64_t size, uint64_t value){
+  bus.bus_store(&(cpu->bus), addr, size, value);
+}
