36985766c67ff19bf532e4163156de33570997bc
ARMontt <alan.r.montt@outlook.com>
2025-02-23 02:20:37 -0500

    Instructions source code fixed

--- include/instructions.h
+++ include/instructions.h
@@ -21,10 +21,10 @@ class I_Inst{
     uint64_t rd       = (uint64_t)  ((i_inst>>7)  &0x1F);
     uint64_t funct3   = (uint64_t)  ((i_inst>>12) &0x07);
     uint64_t rs1      = (uint64_t)  ((i_inst>>15) &0x1F);
-    uint64_t imm11_0  = (uint64_t)  ((i_inst>>20) &0x0FFF; //inst[31:20] = imm[11:0]
+    uint64_t imm11_0  = (uint64_t)  ((i_inst>>20) &0x0FFF); //inst[31:20] = imm[11:0]
 
-    uint64_t get_IMM(I_Inst* inst);
-    uint64_t get_SHAMT(I_Inst* inst);
+    uint64_t I_IMM(I_Inst* inst);
+    uint64_t I_SHAMT(I_Inst* inst);
 };
 
 
@@ -39,7 +39,7 @@ class S_Inst{
     uint64_t rs2      = (uint64_t)  ((s_inst>>20) &0x1F);
     uint64_t imm11_5  = (uint64_t)  ((s_inst>>25) &0x7F);
 
-    uint64_t imm = (uint64_t)(imm11_5<<5)|(imm4_0);
+    uint64_t get_IMM(S_Inst* inst);
 };
 
 
@@ -60,7 +60,7 @@ class B_Inst{
     uint64_t imm10_5      = (imm12_10_5) &0x3F;
     uint64_t imm12        = (imm12_10_5) &0x40;
 
-    uint64_t imm = (uint64_t) (imm12<<12)|(imm11<<11)|(imm10_5<<5)|(imm4_1); //13 bit IMM
+    uint64_t get_IMM(B_Inst* inst); //13 bit IMM
 };
 
 
@@ -72,8 +72,9 @@ class U_Inst{
     uint64_t opcode       = (uint64_t)  (u_inst &0x007F); //Logic AND 16 bits: 0000 0000 0111 1111
     uint64_t rd		  = (uint64_t)	((u_inst>>7)  &0x1F);
     uint64_t imm31_12     = (uint64_t)  ((u_inst>>12) &0x000FFFFF);
+    
+    uint64_t get_IMM(U_Inst* inst); 
 
-    uint64_t imm = imm31_12; //20 bits IMM
 };
 
 
@@ -90,6 +91,6 @@ class J_Inst{
     uint64_t imm10_1	  = (uint64_t)	((imm31_12>>10) &0x3FF);
     uint64_t imm20	  = (uint64_t)  ((imm31_12>>20) &0x01);
 	
-    uint64_t imm = (imm20<<19) | (imm10_1<<9) | (imm11<<8) | (imm19_12); //20 bits IMM
+    uint64_t get_IMM(J_Inst* inst);
 };
 
--- src/instructions.cpp
+++ src/instructions.cpp
@@ -1,10 +1,26 @@
 
 #include "instructions.h"
 
-uint64_t I_Inst::get_IMM(I_Inst* inst){
-	return inst.imm11_0;
+uint64_t I_Inst::I_IMM(I_Inst* inst){
+	return inst->imm11_0;
 }
 
-uint64_t I_Inst::get_IMM(I_Inst* inst){
-	return (inst.imm11_0 & 0x01F);
+uint64_t I_Inst::I_SHAMT(I_Inst* inst){
+	return (inst->imm11_0 & 0x01F);
+}
+
+uint64_t S_Inst::get_IMM(S_Inst* inst){
+  return ((inst->imm11_5<<5)|(imm4_0));
+}
+
+uint64_t B_Inst::get_IMM(B_Inst* inst){
+  return ((inst->imm12<<12)|(inst->imm11<<11)|(inst->imm10_5<<5)|(inst->imm4_1));
+}
+
+uint64_t U_Inst::get_IMM(U_Inst* inst){
+  return (inst->imm31_12);
+}
+
+uint64_t J_Inst::get_IMM(J_Inst* inst){
+  return ((inst->imm20<<19)|(inst->imm10_1<<9)|(imm11<<8)|(imm19_12));
 }
